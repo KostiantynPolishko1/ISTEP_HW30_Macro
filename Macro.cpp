26a82c24975936cf3e2617140b80107ff52851e2
KostiantynPolishko1 <kostantinpolishko@gmail.com>
2023-04-28 15:23:02 +0300

    2 Realized "PR" function for calculation the PI value and use it in macro PI as global const

--- Macro.cpp
+++ Macro.cpp
@@ -1,8 +1,28 @@
 #include<iostream>
 using namespace std;
 
+//Write a program that uses the #define directive to define the PI 
+//constantand calculate the area of the circle. The program should 
+//then ask the user for the radius of the circleand display the area 
+//of the circle.
+
+#define PI PR()
+
+long double PR()
+{
+	long double pi = 0, val = 0, sum = 0;
+
+	for (int i = 0, j = 1; i < 10; i++, j += 2)
+	{
+		(i % 2) ? val = -(1.0 / j) : val = (1.0 / j);
+		sum += val;
+	}
+	return pi = 4 * sum;
+}
+
 int main()
 {
+	cout << PI << endl;
 
 	return 0;
 }
